75e270c6d401ac4045e5cca409d0f0a196b34144
Daniil <danill.novikov@mail.ru>
2022-02-01 01:26:21 +0300

    add Router and Navlink

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,31 +1,39 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Navbar.module.css';
 
-// let s = {
-//     'nav' : 'Navbar_nav__1nYzS',
-//     'item': 'Navbar_item__3kwfZ'
-// }
-
 const Navbar = () => {
-    return <nav className={s.nav}>
-        <div className={s.item}>
-            <a>Profile</a>
-        </div>
-        <div className={s.item}>
-            <a>Messages</a>
-        </div>
-        <div className={s.item}>
-            <a>News</a>
-        </div>
-        <div className={s.item}>
-            <a>Music</a>
-        </div>
-        <div className={s.item}>
-            <a>Settings</a>
-        </div>
+  const setActive = ({ isActive }) => ({ color: isActive ? 'green' : 'white' });
 
+  return (
+    <nav className={s.nav}>
+      <div className={s.item}>
+        <NavLink to="/profile" style={setActive}>
+          Profile
+        </NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="/dialogs" style={setActive}>
+          Messages
+        </NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="/news" style={setActive}>
+          News
+        </NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="/music" style={setActive}>
+          Music
+        </NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="settings" style={setActive}>
+          Settings
+        </NavLink>
+      </div>
     </nav>
+  );
 };
-
 
 export default Navbar;
